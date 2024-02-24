5ba1beb188a262c330f0b018eca80784dcb6ad65
Efraim Andrade <efraim.dev@gmail.com>
2024-02-24 16:35:10 -0300

    feat: setup / add prettier, add husky with lint-staged and  linting project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ export default function Home() {
     <main className="flex h-screen flex-col items-center justify-center">
       <h1 className="text-3xl font-bold">My Next.js Application</h1>
     </main>
-  );
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   plugins: [],
   theme: {
-    extend: {},
+    extend: {}
   },
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-};
-export default config;
+  content: ['./src/**/*.{js,ts,jsx,tsx}']
+}
+export default config
